e46a4f1a43329305d133fd39b467c8444b8ba15a
silfa.wulandari <silfawulandari2704@gmail.com>
2025-04-19 13:57:21 +0700

    Assignment Week 1

--- Project-day-1/hello-world.py
+++ Project-day-1/hello-world.py
@@ -1,1 +1,8 @@
 print("hello world")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+current_year = 2025
+years_until_100 = 100 - age
+year_when_100 = current_year + years_until_100
+
+print(f"{name} will be 100 years old in the year {year_when_100}")
